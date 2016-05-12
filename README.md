644fb323b143f527aa1c0397f5d51973d46134e5
Caitlin <caitlin@cissols.com>
2016-05-12 11:47:49 -0400

    minor bug fixes and added docs

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # AnsiblePlaybookCleaner
-Web application that dynamicly refactors bad playbooks into good
+Web application that dynamically refractors bad playbooks into good
+
+## Usage
+
+Launching the `playbookCleaner.py` file independently will start a publicly available web server on port 8080. You can use the software by accessing the hosted page.
+
+The web app is best used as a uwsgi app behind a proxy like nginx.
+
+## Screen Shots
+
+Step 1.
+Paste in your bad playbook
+![ScreenShot](https://raw.githubusercontent.com/mholiv/AnsiblePlaybookCleaner/devel/meta/img/shot1.png)
+
+Step 2.
+Click Refactor and view the results
+![ScreenShot](https://raw.githubusercontent.com/mholiv/AnsiblePlaybookCleaner/devel/meta/img/shot2.png)
+
+## Recommendations
+
+The app was written on and was designed for pypy3. Other implications of python will most likely work if the prerequisites are available, but this is not tested.
+
+## Known Bugs
+
+The app does not preserve comments.
+
+## Requirements
+- pypy3 or other implementations of python 3.2.5 or later
+- flask 0.10.1. or later
+- PyYAML 3.11 or later
+
+## Author
+AnsiblePlaybookCleaner was written by Caitlin Campbell. You can reach her at cacampbe/redhat/com
--- source/playbookCleaner.py
+++ source/playbookCleaner.py
@@ -7,35 +7,57 @@ import yaml
 import os
 import re
 
+
+# We load up out settings and define the app.
+yml_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.yml')
+with open(yml_location) as settings_file:
+    settings = yaml.load(settings_file)
 app = Flask(__name__)
 
 
-def convert_to_dict(data, trans_func):
+def convert_to_dict(data, trans_worker):
+    """
+    :param data: yaml converted to an array/dictionary by the yaml module
+    :param trans_worker: the worker function (mandatory)
+    :return cleaned up array/dictionary
+    .. note:: Recursive function to parse the yaml tree
+    """
     if isinstance(data, dict):
         for k, v in data.items():
 
             if isinstance(v, dict) or isinstance(v, list) or isinstance(v, tuple):
-                convert_to_dict(v, trans_func)
+                convert_to_dict(v, trans_worker)
 
             else:
-                data[k] = trans_func(v)
+                data[k] = trans_worker(v)
     elif isinstance(data, list) or isinstance(data, tuple):
         for item in data:
-            convert_to_dict(item, trans_func)
+            convert_to_dict(item, trans_worker)
 
     return data
 
 
 def trans_func(value):
+    """
+    :param value: A string consisting of x=y key value pairs
+    :return: A dictionary containing those key value pairs
+    :rtype: dict
+    .. note:: Worker function called by convert_to_dict to convert the string into a dict after a string has been found
+    """
     if '=' in value:
 
         temp_dict = {}
 
         mini_value = re.split('\s', value)
+        last_item_key = ''
         for item in mini_value:
-            mKey = item.split('=')[0]
-            mVal = item.split('=')[1]
-            temp_dict[mKey] = mVal
+            if '=' in item and item.count('=') == 1:
+                m_key = item.split('=')[0]
+                m_val = item.split('=')[1]
+                temp_dict[m_key] = m_val
+                last_item_key = m_key
+            else:
+                temp_dict[last_item_key] = '%s %s' % (temp_dict[last_item_key], item)
 
         value = temp_dict
     return value
@@ -43,14 +65,37 @@ def trans_func(value):
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    """
+    :return: response
+    .. note:: The flask root.
+    """
     if request.method == 'POST':
 
+        # We take the 'bad data' and turn it into good data. Turns out Stannis is wrong.
         good_data = yaml.load(request.form['bad_data'])
-
         rebuilt_data = convert_to_dict(good_data, trans_func)
 
+        # We then take that good rebuilt data and turn it into a string using the yaml module
         refactored_data = yaml.dump(rebuilt_data, width=50, default_flow_style=False)
 
+        # This is messy, but we override some of the areas that the yaml module decided it would quote.
+        # The good news is that it only likes to quote key words and numbers.
+        refactored_data = refactored_data.replace("'''", "'")
+        refactored_data = refactored_data.replace("'\"", "\"")
+        refactored_data = refactored_data.replace("\"'", "\"")
+        refactored_data = refactored_data.replace("'Yes'", "Yes")
+        refactored_data = refactored_data.replace("'yes'", "yes")
+        refactored_data = refactored_data.replace("'No'", "No")
+        refactored_data = refactored_data.replace("'no'", "no")
+        refactored_data = refactored_data.replace("'True'", "True")
+        refactored_data = refactored_data.replace("'False'", "False")
+        refactored_data = refactored_data.replace("'true'", "true")
+        refactored_data = refactored_data.replace("'false'", "false")
+        numbers = re.findall(r'\d+', refactored_data)
+        for number in numbers:
+            refactored_data = refactored_data.replace("'%s'" % number, "%s" % number)
+
+        # We generate and send a response
         response = make_response(refactored_data)
         response.headers["content-type"] = "text/plain"
         return response
@@ -59,12 +104,8 @@ def index():
         return render_template('base.html', refactored_data='')
 
 
+# We start up a server if this script is just launched. In production uwsgi should be used.
 if __name__ == "__main__":
-
-    yml_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.yml')
-    with open(yml_location) as settings_file:
-        settings = yaml.load(settings_file)
-
     app.secret_key = settings['secret_key']
     app.debug = settings['debug']
     app.run(host='0.0.0.0')
