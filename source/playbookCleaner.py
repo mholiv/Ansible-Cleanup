0e23d2f9d51c037ed5ff9da19f96f366ace365a6
Caitlin <caitlin@cissols.com>
2016-05-11 21:20:41 -0400

    working recursive solid model

--- source/playbookCleaner.py
+++ source/playbookCleaner.py
@@ -4,10 +4,41 @@ from flask import request
 from flask import make_response
 
 import yaml
-import collections
 import os
+import re
 
 app = Flask(__name__)
+
+
+def convert_to_dict(data, trans_func):
+    if isinstance(data, dict):
+        for k, v in data.items():
+
+            if isinstance(v, dict) or isinstance(v, list) or isinstance(v, tuple):
+                convert_to_dict(v, trans_func)
+
+            else:
+                data[k] = trans_func(v)
+    elif isinstance(data, list) or isinstance(data, tuple):
+        for item in data:
+            convert_to_dict(item, trans_func)
+
+    return data
+
+
+def trans_func(value):
+    if '=' in value:
+
+        temp_dict = {}
+
+        mini_value = re.split('\s', value)
+        for item in mini_value:
+            mKey = item.split('=')[0]
+            mVal = item.split('=')[1]
+            temp_dict[mKey] = mVal
+
+        value = temp_dict
+    return value
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -16,18 +47,7 @@ def index():
 
         good_data = yaml.load(request.form['bad_data'])
 
-        rebuilt_data = []
-        for task in good_data:
-
-            temp_task = {}
-            for key_value_tup in task.items():
-
-                key = key_value_tup[0]
-                values = key_value_tup[1].split(' ')
-
-                temp_task[key] = {elem.split('=', 1)[0]: elem.split('=', 1)[1] for elem in values}
-
-            rebuilt_data.append(temp_task)
+        rebuilt_data = convert_to_dict(good_data, trans_func)
 
         refactored_data = yaml.dump(rebuilt_data, width=50, default_flow_style=False)
 
@@ -37,8 +57,6 @@ def index():
 
     else:
         return render_template('base.html', refactored_data='')
-
-
 
 
 if __name__ == "__main__":
